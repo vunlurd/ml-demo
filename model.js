0df463cee0648e2211b51dcfe09f27e7ae4e021d
Vunlurd <vunlurd@yahoo.com>
2021-11-19 19:43:00 +0100

    Added example script model.js

---
+++ model.js
@@ -0,0 +1,16 @@
+require('@tensorflow/tfjs');
+const use = require('@tensorflow-models/universal-sentence-encoder');
+
+// Load the model.
+use.load().then(model => {
+  // Embed an array of sentences.
+  const sentences = [
+    'Hello.',
+    'How are you?'
+  ];
+  model.embed(sentences).then(embeddings => {
+    // `embeddings` is a 2D tensor consisting of the 512-dimensional embeddings for each sentence.
+    // So in this example `embeddings` has the shape [2, 512].
+    embeddings.print(true /* verbose */);
+  });
+});
